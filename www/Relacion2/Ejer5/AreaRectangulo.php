e2fe862a8ed9579d6e596ede684a761ffa91e93c
SantiTru <santiagotrujillonavarro@gmail.com>
2023-09-25 21:22:52 +0200

    commit

---
+++ www/Relacion2/Ejer5/AreaRectangulo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Relacion 2 Ejer 6</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+      Hola, el área del rectangulo con base 
+      
+      <?=$_POST["base"]?> 
+
+      y altura 
+
+      <?=$_POST["altura"]?> 
+
+      es: 
+
+      <?php echo $_POST["base"] * $_POST["altura"]; ?>. 
+
+      Que tengas un buenisimo día.
+  
+  </body>
+ </html>
---
+++ www/Relacion2/Ejer6/AreaTriangulo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Relacion 2 Ejer 5</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+      Hola, el área del triangulo con base 
+      
+      <?=$_POST["base"]?> 
+
+      y altura 
+
+      <?=$_POST["altura"]?> 
+
+      es: 
+
+      <?php 
+      $resultado= $_POST["base"] * $_POST["altura"] * 0.5 ;
+      echo ($resultado); 
+      ?>. 
+
+      Que tengas un buenisimo día.
+  
+  </body>
+ </html>
+
+ 
