60e733671a4f7641e3cfed532c5c96a60935ffc9
SantiTru <santiagotrujillonavarro@gmail.com>
2023-09-25 19:10:36 +0200

    commit

--- www/Relacion1/Ejer7/Index.php
+++ www/Relacion1/Ejer7/Index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Ejercicio 7 Relación 1</title>
+    <title>Ejercicio 6 Relación 1</title>
     <meta charset="UTF-8" />
   </head>
   <body>
     <?php
-      echo ("
-        ");  
-      ?>
+      $nombre = "Santiago Trujillo Navarro";
+      $direccion = "Calle ***** ******** N 14";
+      $telefono = "Teléfono 665 *** ***";
+      
+      echo "<h1>$nombre</h1><br>";
+      echo "<h2>$direccion</h2><br>";
+      echo "<h3>$telefono</h3><br>";
+    ?>
   </body>
  </html>
--- www/Relacion1/Ejer8/Index.php
+++ www/Relacion1/Ejer8/Index.php
@@ -5,9 +5,16 @@
     <meta charset="UTF-8" />
   </head>
   <body>
-    <?php
-      echo ("
-        ");  
-      ?>
+      
+<h2 text-align: center>CONVERSOR MONEDA EURO - DOLAR</h2>
+
+<?php
+
+  $euros = 1;
+  $dolares = $euros * 1.07;
+
+  echo "El total de euros <strong>'$euros euros'</strong> a dólares es de <strong>'$dolares dólares'</strong>.";
+
+?>
   </body>
  </html>
