6e6331bda9d0f7e29cf2ba384990d56b500e2984
SantiTru <santiagotrujillonavarro@gmail.com>
2023-10-09 21:14:34 +0200

    Commit

--- www/Relacion3/Relacion3.3/Ejer1/Index.php
+++ www/Relacion3/Relacion3.3/Ejer1/Index.php
@@ -10,22 +10,27 @@
       position: relative;
       width: 1000px;
       height: 200px;
+      text-align: center;
     }
 
     .contenedorhijo {
       width: 400px;
-      height: 400px;
+      height: 800px;
       position: absolute;
       top: 50%;
       left: 50%;
       margin: -25px 0 0 -25px;
-      background-color: khaki;
+      background-color: salmon;
       text-align: center;
     }
-
+                                                                                                                                                                                                        
     .boton {
       text-align: center;
       width: 400px;
+    }
+
+    table {
+      margin: 0 auto; /* Centra la tabla horizontalmente */
     }
   </style>
 </head>
@@ -35,35 +40,32 @@
     <div class="contenedorhijo">
       <br>
       <br>
-      <h3>Vamos a mostrar números aleatorios, cuadrados y cubos de estos números</h3>
+      <h3>Vamos a mostrar 20 números aleatorios y los cuadrados y cubos de estos números</h3>
 
       <?php
-
       $numeros = array();
       $cuadrados = array();
       $cubos = array();
 
-      echo "<table border=1>";
       for ($i = 0; $i < 20; $i++) {
         $numeros[$i] = rand(1, 100);
         $cuadrados[$i] = pow($numeros[$i], 2);
         $cubos[$i] = pow($numeros[$i], 3);
+      }
 
+      echo "<table border=1 aling=center>";
+      echo "<tr><th>Número</th><th>Cuadrado</th><th>Cubo</th></tr>";
+      for ($i = 0; $i < 20; $i++) {
+        echo "<tr>";
+        echo "<td>" . $numeros[$i] . "</td>";
+        echo "<td>" . $cuadrados[$i] . "</td>";
+        echo "<td>" . $cubos[$i] . "</td>";
+        echo "</tr>";
       }
-      foreach ($numeros as $numero) {
-        echo "<tr><td>$numero</td></tr>";
-      }
-      foreach ($cuadrados as $cuadrado) {
-        echo "<tr><td>$cuadrado</td></tr>";
-      }
-      foreach ($cubos as $cubo) {
-        echo "<tr><td>$cubo</td></tr>";
-      } 
       echo "</table>";
       ?>
 
     </div>
   </div>
 </body>
-
 </html>
