f69d42d4c75aee904157185e352eebaf01ee43df
SantiTru <santiagotrujillonavarro@gmail.com>
2024-01-23 18:58:08 +0100

    Commit

---
+++ www/Relacion6/Biblioteca/views/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Querida Biblioteca</title>
+    <link href="./Style/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <h1>MI QUERIDA BIBLIOTECA</h1>
---
+++ www/Relacion6/Biblioteca/views/nav.php
@@ -0,0 +1,8 @@
+<hr/>
+<nav>
+    Menú de navegación: 
+    <a href='index.php'>The Home</a>
+    <a href='index.php?action=mostrarListaLibros'>Libros</a>
+    <a href='index.php?action=mostrarListaAutores'>Autores</a>
+</nav>
+<hr/>
