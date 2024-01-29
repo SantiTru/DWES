658a5338238fddb51083d0ec6714a0e07ebbd0e8
SantiTru <santiagotrujillonavarro@gmail.com>
2024-01-29 20:11:07 +0100

    Commit

--- www/ProyectoTareasMVC/views/header.php
+++ www/ProyectoTareasMVC/views/header.php
@@ -2,9 +2,9 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Mi Querida Biblioteca</title>
+    <title>Atarea2</title>
     <link href="./Style/style.css" rel="stylesheet" />
 </head>
 
 <body>
-    <h1>MI QUERIDA BIBLIOTECA</h1>
+    <h1>Atarea2. Añade, modifica y elimina</h1>
--- www/ProyectoTareasMVC/views/nav.php
+++ www/ProyectoTareasMVC/views/nav.php
@@ -2,7 +2,7 @@
 <nav>
     Menú de navegación: 
     <a href='index.php'>The Home</a>
-    <a href='index.php?action=mostrarListaLibros'>Libros</a>
-    <a href='index.php?action=mostrarListaAutores'>Autores</a>
+    <a href='index.php?action=mostrarListaLibros'>Tareas</a>
+    <a href='index.php?action=mostrarListaAutores'>Tu perfil</a>
 </nav>
 <hr/>
