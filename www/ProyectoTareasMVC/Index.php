86bb7a087f22eaa2927e7a30c2f66035d690cd31
SantiTru <santiagotrujillonavarro@gmail.com>
2024-01-29 20:05:01 +0100

    Commit

--- www/ProyectoTareasMVC/Index.php
+++ www/ProyectoTareasMVC/Index.php
@@ -2,5 +2,6 @@
      @Author: SantiTru
      @version: 1.0
      @date: 2024-01-29
-     @url: https://github.com/SantiTru
+     @url: https://github.com/SantiTru/DWES/tree/main/www/ProyectoTareasMVC
+     @description: Programa básico de tareas con patrón MVC y uso de objetos
 -->
